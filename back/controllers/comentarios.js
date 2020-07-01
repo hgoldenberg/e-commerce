65ac26d347da383463b1ef58cd1d689a4f935794
hgoldenberg <hgoldenberg@hotmail.com>
2020-07-01 11:58:58 -0300

    ruta de comentarios finalizada

---
+++ back/controllers/comentarios.js
@@ -0,0 +1,42 @@
+const obj = {};
+const { Comentarios, Product } = require("../models/index");
+const sequelize = require("sequelize");
+const Op = sequelize.Op;
+
+//creo un comentario
+
+obj.createComentario = (req, res, next) => {
+  console.log("que es",req.body)
+  //if (req.user.id) {
+    Product.findOne({
+      where: {
+        id: req.body.productId
+      }
+    }).then((product) => {
+      const producto = product
+      Comentarios.create( {
+       comentarios: req.body.comentarios
+      })
+      .then((data)=>{
+        data.setUser(req.body.userId)
+        producto.addComentario(data.id) 
+        res.sendStatus(200)
+      })
+    
+  }).catch(next)
+};
+
+// busco todos los comentarios de un producto
+obj.allComentarios = (req, res, next) => {
+  Comentarios.findAll({
+      where:{
+          productId:req.params.productId
+      }
+  })
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch(next);
+};
+
+module.exports = obj;
--- back/routes/index.js
+++ back/routes/index.js
@@ -5,12 +5,14 @@ const users = require('./users');
 const productos = require('./productos');
 const carrito = require('./carrito');
 const categoria = require('./categorias');
+const comentarios = require ('./comentarios')
 
 
 router.use('/users', users);
 router.use('/productos', productos);
 router.use('/carrito' , carrito);
 router.use('/categoria' , categoria);
+router.use ('/comentarios', comentarios);
 
 
 module.exports = router;
